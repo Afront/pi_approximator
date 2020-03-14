dd033fdbad1aef030108d16668819bf7ac87fa5e
Afront <3943720+Afront@users.noreply.github.com>
2020-03-15 01:55:42 +0900

    Refactor Leibniz's formula

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,25 @@ fn leibniz(n: BigInt) -> BigRational {
     let zero = BigInt::new(Sign::Plus, vec![0]);
     let one = BigInt::new(Sign::Plus, vec![1]);
     let two = BigInt::new(Sign::Plus, vec![2]);
+    let four = BigInt::new(Sign::Plus, vec![4]);
 
     let mut total = BigRational::new_raw(Zero::zero(), One::one());
-    let mut i = BigInt::new(Sign::Plus, vec![0]);
+    let mut i = &zero * &zero;
 
     while &i < &n {
-        let left = BigInt::new(Sign::Plus, vec![4]);
-        let right = (&i * &two + &one)
-            * if &i % &two == &zero * &zero {
-                &one * &one
-            } else {
-                &one * -&one
-            };
+        total += BigRational::new_raw(
+            &four * &one,
+            (&i * &two + &one)
+                * if &i % &two == &zero * &zero {
+                    &one + &zero // Probably better than &one * &one
+                } else {
+                    &zero - &one
+                },
+        );
 
-        let new = BigRational::new_raw(left, right);
-        total += new;
         i += &one * &one;
     }
+
     total
 }
 
